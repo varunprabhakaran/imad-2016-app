248cb5ffe2cb6d00d900f2887b4d455e846da41f
varunprabhakaran <varunanie74@gmail.com>
2016-11-26 13:02:15 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,14 +35,17 @@ function createProfil (){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Profile</title>
 
-    <!-- Bootstrap Css-->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="css/myprofile.css">
 
-    <!-- Bootstrap Js -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
 
     <!--Font-awesome cdn-->
 <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">-->
